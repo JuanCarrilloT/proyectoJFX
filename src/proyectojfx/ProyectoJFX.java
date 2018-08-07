585f32f9e654f6e66f9baa801d3ff11c6294164e
PC <PC@DESKTOP-A0B021C>
2018-08-07 17:42:46 -0500

    inicio

--- src/proyectojfx/ProyectoJFX.java
+++ src/proyectojfx/ProyectoJFX.java
@@ -21,7 +21,7 @@ public class ProyectoJFX extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
-        Scene scene = new Scene(root);
+        Scene scene = new Scene (root);
         
         stage.setScene(scene);
         stage.show();
